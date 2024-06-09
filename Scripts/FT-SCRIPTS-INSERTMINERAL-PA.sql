189fbef7f54a8f840c45ec457443faa4b24debf4
paoascanio <131598223+paoascanio@users.noreply.github.com>
2024-06-09 13:45:11 -0500

    Create FT-SCRIPTS-INSERTMINERAL-PA.sql

---
+++ Scripts/FT-SCRIPTS-INSERTMINERAL-PA.sql
@@ -0,0 +1,74 @@
+INSERT INTO MINERAL ( min_nombre, min_medicion, min_formula_quimica, min_pureza_ideal, met_maleabilidad,
+met_dureza, met_tipo_metal, nmet_aislante, min_tipo ) VALUES
+
+( 'Oro', 'Quilates', 'Au', 99, 'Alta', 3, 'No Ferroso', NULL, 'Metalico'),
+
+( 'Diamante', 'Quilates', 'C', 100, NULL, NULL, NULL, 'Electrico', 'No Metalico'),
+
+( 'Hierro', 'Tonelada Metrica', 'Fe', 90, 'Media', 6, 'Ferroso', NULL, 'Metalico'),
+
+('Bauxita', 'Tonelada Metrica', 'AI2O3', 50, NULL, NULL, NULL,'Termico', 'No Metalico'),
+
+('Carbon', 'Tonelada Metrica', 'C', 70, NULL, NULL, NULL, 'Termico', 'No Metalico'),
+
+('Manganeso', 'Tonelada Metrica', 'Mn', 99, 'Media', 7, 'Ferroso', NULL,'Metalico'),
+
+('Niquel', 'Tonelada Metrica','Ni', 99, 'Media', 6, 'No Ferroso',NULL, 'Metalico'),
+
+('Cobre','Tonelada Metrica','Cu', 99, 'Alta', 5, 'No Ferroso',NULL, 'Metalico'),
+
+('Mercurio', 'Gramos', 'Hg', 99,'Alta',1, 'No Ferroso',NULL,'Metalico'),
+
+('Plomo', 'Tonelada Metrica','Pb',99,'Alta', 2,'No Ferroso',NULL,'Metalico'),
+
+('Plata','Gramos','Ag',99,'Alta',3,'No Ferroso',NULL,'Metalico'),
+
+('Bario','Tonelada Metrica','Ba',95,'Media',2,'No Ferroso',NULL,'Metalico'),
+
+('Calcio','Gramos','Ca',99,'Media',2,'No Ferroso',NULL,'Metalico'),
+
+('Caolin','Tonelada Metrica','Al2Si2O5(OH)4',95,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Caliza','Tonelada Metrica','CaCO3',95,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Pirita','Tonelada Metrica','FeS2',90,'Baja',6,'Ferroso',NULL,'Metalico'),
+
+('Talco','Tonelada Metrica','Mg3Si4O10(OH)2',90,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Zinc','Tonelada Metrica','Zn',99,'Media', 2.5, 'No Ferroso',NULL, 'Metalico'),
+
+('Cianita','Tonelada Metrica','Al2SiO5',95,NULL,NULL,NULL,'Electrico','No Metalico'),
+
+('Dolomita','Tonelada Metrica','CaMg(CO3)2',95,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Titanio','Tonelada Metrica','Ti',99,'Alta',5,'No Ferroso',NULL,'Metalico'),
+
+('Tungsteno','Tonelada Metrica','W',99,'Media',8,'No Ferroso',NULL,'Metalico'),
+
+('Magnesio','Tonelada Metrica','Mg',99,'Baja',2,'No Ferroso',NULL,'Metalico'),
+
+('Andalucita','Tonelada Metrica','Al2SiO5',95,NULL,NULL,NULL,'Electrico','No Metalico'),
+
+( 'Antimonio','Tonelada Metrica','Sb',99,'Baja',3.5,'No Ferroso',NULL,'Metalico'),
+
+('Cromo','Tonelada Metrica','Cr',99,'Media',6.5,'No Ferroso',NULL,'Metalico'),
+
+('Asbesto','Tonelada Metrica','MG3Si2H4O9',90,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Feldespato','Tonelada Metrica','XAlSiO8',95,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Arena','Tonelada Metrica','SiO2',95,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Cal','Tonelada Metrica','CaO',95,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Caliza','Tonelada Metrica','CaCO3',94,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Granito','Tonelada Metrica','XAlSiO8',92,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Yeso','Tonelada Metrica','CaSO4·2H2O',95,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Arcilla','Tonelada Metrica','Al2O32SiO22H2O',90,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Baritina','Tonelada Metrica','BaSO4',95,NULL,NULL,NULL,'Termico','No Metalico'),
+
+('Bentonita','Tonelada Metrica','Al2Si3O10(OH)2nH2O',95,NULL,NULL,NULL,'Termico','No Metalico');
