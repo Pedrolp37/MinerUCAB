5a94e85ea4e65bcffb39b9d841b3bf56bd2ea072
paoascanio <131598223+paoascanio@users.noreply.github.com>
2024-06-17 10:35:28 -0500

    SCRIPTS ALIADO COMERCIAL

---
+++ Scripts/FT-SCRIPTS-INSERTALIADO-PA.sql
@@ -0,0 +1,206 @@
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J0000005897', 'CVG Aragua', 'Calle Principal, Maracay, Aragua', '2006-06-17', 1000000.00, '0241-1234567', 'Empresa en Aragua, con más de 15 años en el país.', 4 ); 
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1000002992', 'MinerAragua', 'Avenida Libertador, San Mateo, Aragua', '2002-09-14', 500000.00, '0243-7654321', 'Minería segura para todos .', 4 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J2310001238', 'Minera Amazonas', 'Municipio Autónomo Alto Orinoco, Puerto Ayacucho, Amazonas', '2024-06-17', 3000000.00, '0283-1234567', 'Empresa dedicada a la extracción de oro en Amazonas.', 1 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1590004598', 'Minerales Amazonas', 'Municipio Autónomo Atabapo, San Fernando de Atabapo, Amazonas', '2015-02-20', 2500000.00, '0284-7654321', 'Empresa dedicada a la extracción de diamantes en Amazonas.', 1 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+VALUES ( 'J1100001240', 'Industrias Mineras Anzoátegui', 'Zona Industrial Barcelona, Barcelona, Anzoátegui', '2004-09-17', 4000000.00, '0281-1234567', 'Empresa dedicada a la minería en Anzoátegui.', 2 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+VALUES ( 'J1250001241', 'Mineria Anzoátegui', 'Avenida Arismendi, Lechería, Anzoátegui', '2009-06-17', 3000000.00, '0282-7654321',NULL, 2);
+
+
+
+
+
+
+
+
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1991001242', ' MinerApure', 'Hato El Rincón, Biruaca, Apure', '1992-01-12', 5000000.00, '0257-1234567', NULL, 3 );
+
+
+
+ INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J0000001243', 'MinVen Apure', 'Zona Industrial Guasdualito, Guasdualito, Apure', '1998-02-27', 4000000.00, '0256-7654321', NULL, 3 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+VALUES ( 'J1560201244', 'Minbarina', 'Campo Petrolero Las Moritas, Barinas, Barinas', '2001-02-10', 6000000.00, '0273-1234567', 'Empresa dedicada a la extracción de Oro.', 5 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+ VALUES ( 'J1690001215', 'Minerubi', 'Avenida Bolívar, Barinas, Barinas', '1984-01-17', 5000000.00, '0274-7654321', NULL, 5);
+
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1200002874', 'CVG Industria Venezolana de Aluminio', 'Av. Fuerzas Armadas - Zona Industrial Matanzas. Puerto Ordaz ', '1978-06-10', 7000000.00, '0289-1234567', 'Empresa dedicada a la extracción de oro en Bolívar.', 6 );
+
+ INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+ VALUES ( 'J1580001247', 'Minerboli', 'Municipio Autónomo Cedeño, Caicara del Orinoco, Bolívar', '2012-04-06', 6000000.00, '0288-7654321', 'Empresa dedicada a la extracción de coltán en Bolívar.', 6 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+VALUES ( 'J6980005841', 'Minería Carabobo', 'Sector La Morita, Mariara, Carabobo', '2020-01-08', 5000000.00, '0241-1234567', 'Empresa dedicada a la extracción de bauxita.',7  );
+
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+VALUES ( 'J0000001249', 'Minería Carabobo 2', 'Sector La California, San Diego, Carabobo', '1960-06-06', 4000000.00, '0242-7654321', 'Empresa dedicada a la extracción de caliza.', 7 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+VALUES ( 'J2480001250', 'Minería Cojedes', 'Municipio Autónomo Rómulo Gallegos, San Juan de los Morros, Cojedes', '2002-10-29', 3000000.00, '0258-1234567', 'Empresa dedicada a la extracción de arena en Cojedes.', 8 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+ VALUES ( 'J1110005252', 'Minería Tinaquillo', 'Municipio Autónomo Tinaquillo, Tinaquillo, Cojedes', '2004-01-21', 2000000.00, '0259-7654321', 'Empresa dedicada a la extracción de arcilla en Cojedes.', 8 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+VALUES ( 'J1587000136', 'Minería Delta Amacuro', 'Municipio Autónomo Antonio Díaz, San José de Buja, Delta Amacuro', '2014-03-17', 1500000.00, '0296-1234567', 'Empresa dedicada a la extracción de oro en Delta Amacuro.', 9 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+VALUES ( 'J0000001258', 'Minería ElDelta', 'Municipio Autónomo Pedernales, Pedernales, Delta Amacuro', '2009-09-17', 1000000.00, '0297-7654321', 'Empresa dedicada a la extracción de diamantes en Delta Amacuro.', 9 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+ VALUES ( 'J0000001259', 'MinerUcab', 'Cotiza, Caracas, Distrito Capital', '2007-01-12', 2500000.00, '0212-1234567', null, 10 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+VALUES ( 'J0000001260', 'Minería Urbana Distrito Capital', 'El Cementerio, Caracas, Distrito Capital', '1998-08-17', 2000000.00, '0212-7654321',NULL, 10 );
+
+
+
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+ VALUES ( 'J1569001261', 'Minería Falcón', 'Municipio Autónomo Miranda, Coro, Falcón', '1994-01-07', 3000000.00, '0250-1234567', 'Empresa dedicada a la extracción de caliza en Falcón.', 11 ); 
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1500801262', 'Yesos Falcón', 'Municipio Autónomo Carirubana, Punto Fijo, Falcón', '2003-02-25', 2500000.00, '0251-7654321', 'Empresa dedicada a la extracción de yeso en Falcón.', 11);
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+ VALUES ( 'J1890001263', 'Minería Guárico', 'Municipio Autónomo Camaguán, San Juan de los Morros, Guárico', '2004-04-17', 2000000.00, '0236-1234567', 'Empresa dedicada a la extracción de arena sílice en Guárico.', 12 ); 
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+ VALUES ( 'J1450002995', 'ArciGuarico', 'Municipio Autónomo Valle de la Pascua, Valle de la Pascua, Guárico', '1969-02-20', 1500000.00, '0236-7654321', 'Empresa dedicada a la extracción de arcilla en Guárico.', 12 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1890007895', 'Minería Lara', 'Municipio Autónomo Iribarren, Barquisimeto, Lara', '2006-01-19', 4000000.00, '0251-1234567', 'Empresa dedicada a la extracción de oro en Lara.', 13 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J2680001267', 'MinerLara', 'Municipio Autónomo Jiménez, Quibor, Lara', '2005-07-27', 3500000.00, '0252-7654321', 'Empresa dedicada a la extracción de Oro', 13 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1320001268', 'Minería La Guaira', 'Municipio Autónomo Vargas, La Guaira, La Guaira', '2006-01-23', 2000000.00, '0233-1234567', 'Empresa dedicada a la extracción de arena sílice en La Guaira.', 14 ); 
+
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1520001269', 'La Guairita', 'Municipio Autónomo Vargas, Maiquetía, La Guaira', '2014-12-17', 1500000.00, '0233-7874321', NULL, 14 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+ VALUES ( 'J2650005870', 'Minería Mérida', 'Municipio Autónomo Libertador, Mérida, Mérida', '2007-12-09', 2500000.00, '0274-1234567', 'Empresa dedicada a la extracción de mármol en Mérida.', 15 );
+
+ INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J2992001271', 'Merimineral', 'Municipio Autónomo Rangel, Mucuchíes, Mérida', '2009-02-11', 2000000.00, '0274-7654321', 'Empresa dedicada a la extracción de granito en Mérida.', 15 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1268001852', 'Minería Miranda', 'Municipio Autónomo Chacao, Chacao, Miranda', '2004-09-17', 3000000.00, '0212-1237867', 'Empresa dedicada a la extracción de arena en Miranda.', 16);
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J2580001274', 'Mirandina', 'Municipio Autónomo Zamora, Guatire, Miranda', '2002-08-20', 2000000.00, '0212-7812567', 'Empresa dedicada a la extracción de arcilla en Miranda.', 16 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1212001585', 'Minería Monagas', 'Municipio Autónomo Maturín, Maturín, Monagas', '2014-06-28', 2500000.00, '0292-2987567', 'Empresa dedicada a la extracción de bauxita en Monagas.', 17 ); 
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+ VALUES ( 'J0000001276', 'MinerMonagas', 'Municipio Autónomo Libertador, Caripito, Monagas', '2013-02-07', 2000000.00, '0292-7814321', 'Empresa dedicada a la extracción de oro en Monagas.', 17 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J0000001277', 'Minería Nueva Esparta', 'Municipio Autónomo Arismendi, Porlamar, Nueva Esparta', '2024-06-17', 3000000.00, '095-1234567', 'Empresa dedicada a la extracción de caliza en Nueva Esparta.', 18 ); 
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J0000001278', 'Minería Nueva Esparta 2', 'Municipio Autónomo Maneiro, Pampatar, Nueva Esparta', '2024-06-17', 2500000.00, '096-7654321', 'Empresa dedicada a la extracción de arena sílice en Nueva Esparta.', 18 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+VALUES ( 'J1690001244', 'PortuOro', 'Municipio Autónomo Guanare, Guanare, Portuguesa', '1984-10-10', 2000000.00, '0257-1234567', 'Empresa dedicada a la extracción de Oro en Portuguesa.', 19 );
+
+ INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+VALUES ( 'J1350005890', 'Minería Portuguesa', 'Municipio Autónomo Biruaca, Biruaca, Portuguesa', '1984-02-17', 1500000.00, '0257-7654321', 'Empresa dedicada a la extracción de arena Bauxita.', 19);
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1680001281', 'MingenSu', 'Municipio Autónomo Cumaná, Cumaná, Sucre', '03-05-1969', 2500000.00, '0291-4598567',NULL, 20 );
+
+ INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J2500005841', 'Subauxita', 'Municipio Autónomo Andrés Mata, San Cristóbal, Sucre', '2002-04-09', 2000000.00, '0291-7658741', NULL, 20 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J2130005841', 'TachiIndustrial', 'Municipio Autónomo San Cristóbal, San Cristóbal, Táchira', '2006-08-27', 3000000.00, '0276-7811248', NULL, 21 ); 
+
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+ VALUES ( 'J1585076312', 'MinerGochos', 'Municipio Autónomo Andrés Bello, Cordero, Táchira', '1976-12-12', 2500000.00, '0276-7654321',NULL, 21 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J2992251275', 'MinerTruji', 'Municipio Autónomo Valera, Valera, Trujillo', '06/09/2003', 2000000.00, '0271-4618667', 'Empresa dedicada y responsable en el país.', 22);
+
+ INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+ VALUES ( 'J1353641286', 'FundiVen', 'Municipio Autónomo Boconó, Boconó, Trujillo', '2001-12-01', 1500000.00, '0271-7816670', NULL, 22 );
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1110007889', 'Yaracimiento', 'Municipio Autónomo San Felipe, San Felipe, Yaracuy', '29-06-1985', 2500000.00, '0253-1234567',NULL, 23 ); 
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id ) 
+
+VALUES ( 'J1354871290', 'Minería Yaracuy', 'Municipio Autónomo Nirgua, Nirgua, Yaracuy', '2024-06-17', 2000000.00, '0254-7524721',null, 23);
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+ VALUES ( 'J1163151220', 'Minería Zulia', 'Municipio Autónomo Maracaibo, Maracaibo, Zulia', '1950-06-18', 3500000.00, '0261-1234567',null, 24 ); 
+
+INSERT INTO ALIADO_COMERCIAL ( ali_RIF, ali_nombre, ali_direccion, ali_fecha_creacion, ali_capital, ali_num_telefono, ali_descripcion, fk_lu_id )
+
+ VALUES ( 'J5890001288', 'MinerVen', 'Municipio Autónomo Cabimas, Cabimas, Zulia', '2024-06-17', 3000000.00, '0261-8974321', null, 24 );
+
+
+
