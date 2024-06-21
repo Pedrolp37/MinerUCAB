dced0d7e6b22f2caaa175122109bb7f19c22f762
paoascanio <131598223+paoascanio@users.noreply.github.com>
2024-06-21 17:34:53 -0500

    SOLICITUD CLIENTE 15 INSERTS

---
+++ Scripts/FT-SCRIPTS-SOLICITUD CLIENTE.sql
@@ -0,0 +1,17 @@
+INSERT INTO SOLICITUD_CLIENTE (factura_cli_fecha, factura_cli_total, factura_cli_observacion, factura_fk_cl_identificacion)
+VALUES
+('2024-04-02', 12345.67, 'Factura para cliente V115345678', 'V115345678'),
+('2024-04-07', 98765.43, 'Factura para cliente V134567890', 'V134567890'),
+('2024-04-15', 54321.09, 'Factura para cliente J154321098', 'J154321098'),
+('2024-04-22', 67890.12, 'Factura para cliente J121098765', 'J121098765'),
+('2024-04-28', 32109.87, 'Factura para cliente V192203835', 'V192203835'),
+('2024-05-04', 87654.32, 'Factura para cliente J121098765', 'J121098765'),
+('2024-05-11', 21098.76, 'Factura para cliente V241789632', 'V241789632'),
+('2024-05-19', 76543.21, 'Factura para cliente V172741236', 'V172741236'),
+('2024-05-25', 43210.98, 'Factura para cliente V112345678', 'V112345678'),
+('2024-06-01', 09876.54, 'Factura para cliente J187650321', 'J187650321'),
+('2024-06-09', 65432.10, 'Factura para cliente V242680135', 'V242680135'),
+('2024-06-16', 98765.43, 'Factura para cliente V197531486', 'V197531486'),
+('2024-06-02', 54321.09, 'Factura para cliente V178965412', 'V178965412'),
+('2024-06-17', 32109.87, 'Factura para cliente V287654323', 'V287654323'),
+('2024-06-10', 87654.32, 'Factura para cliente V178965412', 'V178965412');
