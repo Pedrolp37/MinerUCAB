c9b9ed8468c08ce061c44d4dd11ebfe63f97c313
paoascanio <131598223+paoascanio@users.noreply.github.com>
2024-06-21 17:21:44 -0500

    SOLICITUD ALIADO

---
+++ Scripts/FT-SCRIPTS-INSERT-SOLICITUD_aLIADO.sql
@@ -0,0 +1,69 @@
+INSERT INTO SOLICITUD_ALIADO (  factura_ali_fecha, factura_ali_total, factura_ali_observacion,  factura_fk_ali_RIF,  factura_fk_pro_id)
+VALUES
+
+('2024-04-20', 445845.67, 'Factura para aliado  J0000005897',  'J0000005897', 771),
+
+('2024-06-11', 545321.00, 'Factura para aliado J1000002992', 'J1000002992', 772),
+
+('2024-05-20', 98765.43, 'Factura para aliado J2310001238', 'J2310001238',773),
+('2024-05-25', 23456.78, 'Factura para aliado COMERCIAL', 'J1590004598', 774),
+('2024-06-05', 67890.12, 'Factura para aliado Comercial', 'J1100001240', 775),
+('2024-06-10', 45678.90, 'Factura para aliado Comercial', 'J1250001241', 776),
+('2024-06-15', 32109.87, 'Factura para aliado Comercial', 'J1991001242', 777),
+('2024-06-18', 76543.21, 'Factura para aliado Comercial', 'J0000001243', 778),
+('2024-06-20', 10987.65, 'Factura para aliado Comercial', 'J0000005897', 779),
+('2024-06-14', 54321.98, 'Factura para aliado Comercial', 'J1560201244', 780);
+
+
+
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id)
+VALUES
+
+('2024-06-07', 445845.67, 'Factura para aliado', 'J1690001215', 781),
+('2024-06-14', 545321.00, 'Factura para aliado', 'J1200002874', 782),
+('2024-06-19', 98765.43, 'Factura para aliado', 'J1580001247', 783),
+('2024-06-14', 23456.78, 'Factura para aliado COMERCIAL', 'J6980005841', 784),
+('2024-04-08', 67890.12, 'Factura para aliado Comercial', 'J0000001249', 785),
+('2024-04-15', 45678.90, 'Factura para aliado Comercial', 'J2480001250', 786),
+('2024-04-22', 32109.87, 'Factura para aliado Comercial', 'J1110005252', 787),
+('2024-04-29', 76543.21, 'Factura para aliado Comercial', 'J1587000136', 788),
+('2024-05-06', 10987.65, 'Factura para aliado Comercial', 'J0000001258', 789),
+('2024-05-13', 54321.98, 'Factura para aliado Comercial', 'J0000001259', 790),
+('2024-04-05', 445845.67, 'Factura para aliado', 'J0000001260', 791),
+('2024-04-12', 545321.00, 'Factura para aliado', 'J1500801262', 792),
+('2024-04-20', 98765.43, 'Factura para aliado', 'J1890001263', 793),
+('2024-04-27', 23456.78, 'Factura para aliado COMERCIAL', 'J1450002995', 794),
+('2024-05-03', 67890.12, 'Factura para aliado Comercial', 'J1890007895', 795),
+('2024-05-10', 45678.90, 'Factura para aliado Comercial', 'J2680001267', 796),
+('2024-05-17', 32109.87, 'Factura para aliado Comercial', 'J1320001268', 797),
+('2024-05-22', 76543.21, 'Factura para aliado Comercial', 'J1520001269', 798),
+('2024-05-28', 10987.65, 'Factura para aliado Comercial', 'J2650005870', 799),
+('2024-06-02', 54321.98, 'Factura para aliado Comercial', 'J2992001271', 800);
+
+
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id)
+VALUES
+
+('2024-05-18', 765432.10, 'Factura para aliado', 'J1268001852', 801),
+('2024-05-23', 321098.76, 'Factura para aliado', 'J2580001274', 802),
+('2024-05-29', 654321.09, 'Factura para aliado', 'J1212001585', 803),
+('2024-06-03', 987654.32, 'Factura para aliado', 'J0000001276', 804),
+('2024-06-08', 543210.87, 'Factura para aliado', 'J0000001277', 805),
+('2024-06-13', 210987.65, 'Factura para aliado', 'J0000001278', 806),
+('2024-06-18', 876543.21, 'Factura para aliado ', 'J1690001244', 807),
+('2024-06-03', 432109.87, 'Factura para aliado', 'J1350005890', 808),
+('2024-06-08', 765432.10, 'Factura para aliado', 'J1680001281', 809),
+('2024-06-03', 321098.76, 'Factura para aliado', 'J2500005841', 810),
+('2024-04-01', 743216.54, 'Factura para aliado', 'J2130005841', 811),
+('2024-04-04', 987654.32, 'Factura para aliado', 'J1585076312', 812),
+('2024-04-09', 321098.76, 'Factura para aliado', 'J2992251275', 813),
+('2024-04-14', 654321.09, 'Factura para aliado', 'J1353641286', 814),
+('2024-04-19', 098765.43, 'Factura para aliado', 'J1110007889', 815),
+('2024-04-24', 543210.87, 'Factura para aliado', 'J1354871290', 816),
+('2024-04-30', 210987.65, 'Factura para aliado', 'J1163151220', 817),
+('2024-05-02', 876543.21, 'Factura para aliado', 'J5890001288', 818),
+('2024-05-07', 432109.87, 'Factura para aliado', 'J1680001281', 819),
+('2024-05-12', 098765.43, 'Factura para aliado', 'J0000001277', 820);
+
