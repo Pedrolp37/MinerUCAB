91843d75fc08595668c1da32374996331126eaad
paoascanio <131598223+paoascanio@users.noreply.github.com>
2024-06-21 16:36:37 -0500

    FT-INSERTS-PROYECTO
    PROYECTO INSERTS

---
+++ Scripts/FT-SCRIPTS-INSERTPROYECTO-PA.sql
@@ -0,0 +1,252 @@
+INSERT INTO PROYECTO (pro_nombre, pro_descripcion, pro_fecha_ini, pro_fecha_fin, pro_fk_min_id,pro_fk_po_id)
+
+VALUES ('Explotación Minera de Oro', 'Explotación del yacimiento de oro con alta concentración', '2024-07-17', '2024-12-31', 1, 1),
+
+  ('Exploración de Oro', 'Exploración de un yacimiento de Oro',   '2024-08-01', '2025-01-31', 1, 2),
+
+  ('MinerOro', 'Exploración perforación y explotación de un yacimiento de Oro', '2024-07-01', '2025-02-21', 1, 3),
+
+  ('Exploración Minera de Diamante', 'Exploración de una fuente de Diamante', '2024-07-01', '2025-03-31', 2, 4),
+
+  ('DiamantesUcab', 'Explotación de la fuente rica en Diamante de calidad gemológica', '2024-08-21', '2025-04-30', 2, 5),
+
+  ('Extracción de Diamantes', 'Extracción de la fuente rica en Diamantes', '2024-08-11', '2025-05-31', 2, 6),
+
+  ('Exploración Minera de Hierro', 'Exploración de un depósito extenso de Hierro utilizado en siderurgia', '2024-09-21', '2025-06-30', 3, 7),
+
+  ('Extracción Minera de Hierro', 'Extracción de un yacimiento de hierro', '2024-07-01', '2025-07-31', 3, 8),
+
+  ('Hierro Ucab', 'Proyecto para extracción de hierro en el país', '2024-08-01', '2025-08-31', 3, 9),
+
+  ('Exploración Minera de Bauxita', 'Exploración de un yacimiento de bauxita', '2024-09-01', '2025-09-30', 4, 10),
+
+
+  ('MinerBauxita', 'Proyecto dedicado a la Bauxita', '2024-07-15', '2025-07-15', 4, 11),
+
+
+  ('Extracción de Bauxita ', 'Extracción de un yacimiento de bauxita', '2024-08-04', '2025-08-30', 4, 12),
+
+('Minera de Carbón', 'Explotación de carbón mineral', '2024-10-12', '2025-12-31', 5, 13),
+
+('Exploración de Carbón', 'Exploración de carbón en Zulia', '2024-07-01', '2026-12-20', 5, 14),
+
+('Minera de Carbón', 'Explotación de carbón mineral', '2024-07-15', '2025-12-15', 5, 15),
+
+
+('Manganeso Upata', 'Explotación de mineral de manganeso en el yacimiento de Upata', '2024-09-01', '2026-01-31', 6, 16),
+
+('Explotación Manganeso', 'Explotación de mineral de manganeso en Venezuela', '2024-10-01', '2026-10-01', 6, 17),
+
+('Extracción del Manganeso', 'Explorando y extrayendo manganeso', '2024-12-01', '2027-12-01', 6, 18),
+
+ ('Níquel Ucab', 'Explotación mineral de níquel en el yacimiento la Loma', '2025-03-01', '2027-02-28', 7, 19),
+
+ (' Exploración Níquel Loma', 'Exploración de mineral de níquel en el yacimiento Loma', '2024-06-30', '2030-06-30', 7, 20),
+
+ ('Extrayendo Níquel', 'Extracción del Níquel en Venezuela', '2024-10-21', '2027-10-21', 7, 21),
+
+('Proyecto Explotación de Cobre', 'Explotación de mineral de cobre en el yacimiento Las Breñas', '2024-06-01', '2028-03-31', 8, 22),
+
+('Explorando Cobre ', 'Exploración de mineral de cobre en los yacimientos Brisas y Cristinas', '2024-09-12', '2027-04-30', 8, 23),
+
+('Cobre Loma Larga', 'Extracción de cobre en el yacimiento Loma Larga', '2024-06-21', '2026-05-31', 8, 24),
+
+
+
+
+
+('Mercurio Mundo Minero', 'Explotación de mineral de mercurio en en Venezuela', '2025-07-01', '2029-06-30', 9, 25),
+
+('Mercurio Mineral', 'Exploración  y explotación de mineral de mercurio en Venezuela', '2025-08-01', '2029-07-31', 9, 26),
+
+('Mercurio Minero Venezolano', 'Explotación de mineral de mercurio en Venezuela', '2025-09-01', '2029-08-31', 9, 27),
+
+('Plomo Yacimiento Minero', 'Explotación de mineral de plomo en Venezuela', '2025-10-01', '2029-09-30', 10, 28),
+
+('Plomo Minero', 'Extracción de mineral de plomo ', '2025-11-01', '2029-10-31', 10, 29),
+
+('Plomo Minería UCAB', 'Explotación de mineral de plomo con alta responsabilidad', '2025-12-01', '2029-11-30', 10, 30),
+
+('Plata Minera', 'Explotación de mineral de plata en Venezuela', '2026-01-01', '2029-12-31', 11, 31),
+
+('Plata Minero', 'Explotación de mineral de plata en Venezuela', '2026-02-01', '2030-01-31', 11, 32),
+
+('Proyecto de Plata', 'Explotación de mineral de plata en Venezuela', '2026-03-01', '2030-02-28', 11, 33),
+
+
+ ('Bario Venezuela', 'Explotación de mineral de bario', '2012-04-01', '2023-03-31', 12, 34),
+
+('Bario Proyecto', 'Explotación de mineral de bario en el yacimiento', '2023-05-01', '2025-04-30', 12, 35),
+
+('Bario Minero', 'Explotación de mineral de bario', '2023-06-01', '2034-05-31', 12, 36),
+
+('Calcio Venezolan', 'Extracción de calcio en el yacimiento ', '2024-07-20', '2034-06-30', 13, 37),
+
+('Extrayendo Calcio', 'Explotacion de calcio en Venezuela responsablemente', '2023-08-01', '2029-07-31', 13, 38),
+
+('Calcio Miner', 'Explotación en el yacimiento de Venezuela', '2022-12-12', '2026-08-11', 13, 39),
+
+
+
+
+
+('Vene Caolín', 'Extracción de mineral de caolín en el yacimiento ', '2024-06-15', '2028-05-31', 14, 40),
+
+('Caolín Proyecto', 'Exploracion y extracción de mineral de caolín ', '2023-01-01', '2027-06-30', 14, 41),
+
+('Caolín Miner', 'Explotación de mineral de caolín en yacimientos venezolanos', '2023-12-01', '2026-07-31', 14, 42),
+
+('Caliza Venezolana', 'Extracción de piedra caliza en el yacimiento ', '2024-07-20', '2034-06-30', 15, 43),
+
+('Extrayendo Caliza', 'Explotación de piedra caliza en Venezuela responsablemente', '2023-08-01', '2029-07-31', 15, 44),
+
+('Caliza Patanemo', 'Explotación de piedra caliza en el yacimiento de Patanemo', '2022-12-12', '2026-08-11', 15, 45),
+
+('Extrayendo Pirita', 'Extracción de mineral de pirita en el yacimiento ', '2024-06-15', '2028-05-31', 16, 46),
+
+('Pirita Venezolana', 'Exploración y extracción de mineral de pirita ', '2023-01-01', '2027-06-30', 16, 47),
+
+('Pirita Miner', 'Explotación de mineral de pirita en yacimientos venezolanos', '2023-12-01', '2026-07-31', 16, 48),
+
+('Talco Cabimba', 'Explotación de mineral de talco en el yacimiento Cabimba', '2024-09-01', '2025-08-31', 17, 49),
+
+('Talco Agua Fría', 'Explotación de mineral de talco en el yacimiento Agua Fría', '2022-10-11', '2024-09-30', 17, 50),
+
+('Talco Miner', 'Explotación de mineral de talco en yacimientos de Venezuela', '2024-12-25', '2028-10-31', 17, 51),
+
+('Zinc Perijá', 'Explotación de mineral de zinc en yacimientos de Perijá', '2023-05-21', '2028-01-20', 18, 52),
+
+('Zinc Los Bailadores', 'Extracción de mineral de zinc en el yacimiento Bailadores Venezuela', '2024-12-15', '2028-12-31', 18, 53),
+
+('Zinc MINERO', 'Explotación de mineral de zinc ', '2026-02-01', '2030-01-31', 18, 54),
+
+
+('Cianita Miner', 'Explotación de mineral de cianita ', '2025-03-01', '2029-02-28', 19,55),
+
+('Cianita Proyecto', 'Explotación de mineral de cianita ', '2024-04-01', '2028-03-31', 19, 56),
+
+('Cianita Mineral', 'Explotación de mineral de cianita', '2023-05-01', '2027-04-30', 19, 57),
+
+ ('Dolomita Guacuripia', 'Explotación de mineral de dolomita en el yacimiento Guacuripia', '2022-06-01', '2024-05-31', 20, 58),
+
+('Dolomita Mineral', 'Explotación de mineral de dolomita en el yacimiento', '2022-07-01', '2026-06-30', 20, 59),
+
+('Dolomita El Palmar', 'Explotación de mineral de dolomita en el yacimiento El Palmar', '2023-08-01', '2024-07-31', 20, 60),
+
+
+
+
+
+
+('Titanio San Quintín', 'Explotación de mineral de titanio en el yacimiento San Quintín', '2023-09-25', '2030-08-31', 21, 61),
+
+('Titanio Proyecto', 'Explotación de mineral de titanio', '2025-10-31', '2032-09-30', 21, 62),
+
+('Titanio Venezolano', 'Explotación de mineral de titanio ', '2024-11-05', '2025-10-15', 21, 63),
+
+('Tungsteno Los Pijiguaos', 'Explotación de mineral de tungsteno en el yacimiento Los Pijiguaos', '2026-02-21', '2030-11-30', 22, 64),
+
+('Tungsteno El Callao', 'Explotación de mineral de tungsteno en el yacimiento El callao', '2025-01-01', '2029-12-31', 22, 65),
+
+('Tungsteno Mina Venezolana', 'Explotación de mineral de tungsteno en Venezuela', '2024-12-14', '2028-12-31', 22, 66),
+
+  ('Proyecto Magnesio ', 'Explotación de mineral de magnesio en Venezuela', '2023-04-01', '2026-03-31', 23, 67),
+
+  ('Proyecto Mineral Magnesio', 'Explotación de mineral de magnesio', '2023-05-01', '2025-04-30', 23, 68),
+
+  ('Extrayendo Magnesio ', 'Explotación de mineral de magnesio', '2023-06-01', '2024-05-31', 23, 69),
+
+
+  ('Proyecto Andalucita', 'Explotación de mineral de andalucita en Venezuela', '2023-07-01', '2026-06-30', 24, 70),
+
+  ('Extrayendo Andalucita', 'Explotación de mineral de andalucita', '2023-08-01', '2026-07-31', 24, 71),
+
+  ('Miner Andalucita', 'Explotación de mineral de andalucita ', '2023-09-01', '2025-08-31', 24, 72),
+
+('Antimonio Mineral', 'Explotación de mineral de antimonio en Yaracuy', '2023-10-20', '2030-09-30', 25, 73),
+
+('Antimonio Venezolano', 'Explotación de mineral de antimonio en Venezuela', '2024-11-25', '2026-10-31', 25, 74),
+
+('Antimonio Proyecto', 'Explotación y exploración de mineral de antimonio de forma segura y responsable', '2024-12-12', '2028-11-30', 25, 75),
+
+
+('Cromo Proyecto', 'Explotación de mineral de cromo en Venezuela', '2022-12-21', '2026-01-31', 26, 76),
+
+('Cromo Urimán', 'Explotación de mineral de cromo en Urimán', '2025-04-09', '2030-08-28', 26, 77),
+
+('Cromo Paraytepuy', 'Explotación de mineral de cromo en Paraytepui', '2023-04-01', '2024-03-31', 26, 78),
+
+('Asbesto Proyecto', 'Explotacion de mineral de Asbesto en Venezuela', '2022-12-21', '2026-01-31', 27, 79),
+
+('Asbesto Minero', 'Extracción de mineral de Asbesto', '2025-04-09', '2030-08-28', 27, 80),
+
+('Asbesto Proyecto', 'Explotación de mineral de cromo en Paraytepui', '2023-04-01', '2024-03-31', 27, 81),
+
+('Feldespato La Quebrada', 'Explotación de mineral de feldespato en La Quebrada', '2022-05-01', '2030-04-30', 28, 82),
+
+('Feldespato Las Cuevas', 'Explotación de mineral de feldespato en Las Cuevas', '2023-12-01', '2025-07-31', 28, 83),
+
+('Feldespato Minero', 'Explotación de mineral de feldespato en Venezuela', '2023-11-20', '2026-06-30', 28, 84),
+
+
+('Arena Proyecto', 'Extracción de arena en el país ', '2024-08-01', '2030-07-31', 29, 85),
+
+('Arena Venezuela', 'Extracción de arena', '2022-09-01', '2029-08-31', 29, 86),
+
+('Arena Miner', 'Explotación de arena', '2024-12-20', '2028-09-30', 29, 87),
+
+
+('Cal Minera', 'Producción de cal hidratada a partir de piedra caliza', '2023-12-01', '2027-11-30', 30, 88),
+
+ ('Cal Proyectito', 'Producción de cal agrícola a partir de piedra caliza', '2022-11-01', '2026-10-31', 30, 89),
+
+ ('Cal Patanemo', 'Producción de cal industrial a partir de piedra caliza', '2024-10-01', '2025-09-30', 30, 90),
+
+('Caliza Tumeremo', 'Explotación de piedra caliza para la producción de cal', '2023-09-01', '2024-08-31', 31, 91),
+
+('Caliza Clarines', 'Explotación de piedra caliza en Venezuela', '2021-08-01', '2023-07-31', 31, 92),
+
+('Caliza Proyecto', 'Explotación de piedra caliza', '2021-07-01', '2024-06-30', 31, 93),
+
+
+('Granito San Carlos', 'Explotación de granito para la construcción en yacimientos de granito en San CARLOS', '2024-06-01', '2030-05-31', 32,106 ),
+
+('Granito Proyecto en Cojedes', 'Explotación de granito', '2026-05-01', '2030-04-30', 32, 107),
+
+('Granito Ucab', 'Explotación de granito en Venezuela', '2021-04-01', '2023-03-31', 32, 108),
+
+
+('Yeso Venezuela', 'Explotación de yeso en toda Venezuela', '2023-12-21', '2027-01-31', 33, 94),
+
+('Yeso Miner Taracoa', 'Explotación de yeso en Taracoa', '2024-05-29', '2026-05-31', 33, 95),
+
+('Yeso Proyecto Quebrada Grande', 'Explotación de yeso en Quebrada Grande', '2023-12-11', '2025-11-30', 33, 96),
+
+
+ ('Arcilla Cubagua', 'Explotación de arcilla roja para la fabricación de ladrillos', '2022-11-22', '2024-10-31', 34, 97),
+
+('Arcilla Isla Coche', 'Explotación de arcilla de forma responsable', '2021-10-01', '2025-09-30', 34, 98),
+
+('Arcilla Proyecto Venezuela', 'Explotación de arcilla en Venezuela', '2024-09-11', '2032-08-31', 34, 99),
+
+
+
+
+
+('Explorando Baritina ', 'Exploración y Explotación de baritina ', '2022-08-17', '2023-02-20', 35, 100),
+
+ ('Baritina Extracción', ' explotación de baritina de forma responsable', '2021-07-19', '2024-06-30', 35, 101),
+
+('Baritina Minervenezuela', 'Explotación de baritina en Venezuela', '2025-06-01', '2029-05-31', 35, 102),
+
+('Bentonita Matorral', 'Explotación de bentonita en el yacimiento Matorral Estado Monagas', '2024-01-01', '2028-12-31', 36, 103),
+
+('Bentonita Barinas', 'Explotación de bentonita en el yacimiento Barinas Estado Barinas', '2023-12-01', '2027-11-30', 36, 104),
+
+('Bentonita Zulia', 'Explotación de bentonita en el yacimiento Zulia Estado Zulia', '2022-11-01', '2026-10-31', 36, 105),
+
+('Azufre Morichal', 'Explotación de azufre en el yacimiento Morichal Estado Monagas', '2021-10-01', '2025-09-30', 37, 109),
+
+('Azufre Cerro Amarillo', 'Explotación de azufre en el yacimiento Cerro Amarillo Estado Bolívar', '2020-09-01', '2024-08-31', 37, 110),
+
+('Azufre Mérida', 'Explotación de azufre en el yacimiento Mérida Estado Mérida', '2019-08-01', '2023-07-31', 37, 111);
