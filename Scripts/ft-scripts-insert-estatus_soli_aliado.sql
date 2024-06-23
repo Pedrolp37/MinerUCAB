93bdff00021ccf52bb91262edf56aa9ed4d64f22
paoascanio <131598223+paoascanio@users.noreply.github.com>
2024-06-23 11:54:24 -0500

    FT SCRIPTS ESTATUS SOLICITUD ALIADO

---
+++ Scripts/ft-scripts-insert-estatus_soli_aliado.sql
@@ -0,0 +1,166 @@
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin) 
+VALUES (101, 18, '2024-04-20', '2024-06-20'); 
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin) 
+ VALUES (102, 17, '2024-06-11', '2024-06-22'); 
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+ VALUES (103, 16, '2024-05-20', '2024-06-22'); 
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin) 
+ VALUES (104, 18, '2024-05-25', '2024-05-29'); 
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+ VALUES (105, 16, '2024-06-05', '2024-06-18');
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+ VALUES (106, 17, '2024-06-10', '2024-06-19');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin) 
+VALUES (107, 17, '2024-06-15', '2024-06-19'); 
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+ VALUES (108, 16, '2024-06-18', '2024-06-22'); 
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin) 
+ VALUES (109, 16, '2024-06-20', '2024-06-23');
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+ VALUES (110, 17, '2024-06-14', '2024-06-23'); 
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+ VALUES (111, 17, '2024-06-07', '2024-06-21'); 
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+ VALUES (112, 17, '2024-06-14', '2024-06-22');
+
+ INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (113, 16, '2024-06-19', '2024-06-20');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (114, 17, '2024-06-14', '2024-06-23');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (115, 18, '2024-04-08', '2024-06-08');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (116, 18, '2024-04-15', '2024-06-15');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (117, 18, '2024-04-22', '2024-06-22');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (118, 17, '2024-04-29', '2024-06-21');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (119, 18, '2024-05-06', '2024-06-06');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (120, 17, '2024-05-13', '2024-06-13');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (121, 17, '2024-04-05', '2024-06-15');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (122, 18, '2024-04-12', '2024-05-12');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (123, 18, '2024-04-20', '2024-06-20');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (124, 18, '2024-04-27', '2024-05-27');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (125, 17, '2024-05-03', '2024-06-03');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (126, 17, '2024-05-10', '2024-06-10');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (127, 18, '2024-05-17', '2024-06-17');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (128, 17, '2024-05-22', '2024-06-22');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (129, 17, '2024-05-28', '2024-06-22');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (130, 16, '2024-06-02', '2024-06-12');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (131, 17, '2024-05-18', '2024-06-18');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (132, 18, '2024-05-23', '2024-06-23');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (133, 17, '2024-05-29', '2024-06-23');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (134, 16, '2024-06-03', '2024-06-23');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (135, 16, '2024-06-08', '2024-06-18');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (136, 17, '2024-06-13', '2024-06-23');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (137, 16, '2024-06-18', '2024-06-23');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (138, 17, '2024-06-03', '2024-06-23');
+
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (139, 16, '2024-06-08', '2024-06-21');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (140, 16, '2024-06-03', '2024-06-15');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (141, 18, '2024-04-01', '2024-05-01');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (142, 18, '2024-04-04', '2024-06-04');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (143, 18, '2024-04-09', '2024-06-09');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (144, 18, '2024-04-14', '2024-06-14');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (145, 18, '2024-04-19', '2024-05-19');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (146, 17, '2024-04-24', '2024-06-18');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (147, 18, '2024-04-30', '2024-05-30');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (148, 17, '2024-05-02', '2024-06-02');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (149, 18, '2024-05-07', '2024-05-30');
+
+INSERT INTO EST_SOLICITUD (est_sol_fk_sol_ali, est_sol_fk_est_id, est_sol_fecha_ini, est_sol_fecha_fin)
+VALUES (150, 17, '2024-05-12', '2024-06-08');
+
+
+ 
+ 
