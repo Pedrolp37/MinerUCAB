492490c599a04443e38f09c61d47500c5e745dea
Pedrolp37 <56178798+Pedrolp37@users.noreply.github.com>
2024-07-07 14:48:53 -0400

    ingresar solicitud aliado

--- BackEnd/src/controllers/solicitudes.controllers.js
+++ BackEnd/src/controllers/solicitudes.controllers.js
@@ -23,3 +23,35 @@ export const postSolicitudCliente = async(req,res) =>{
         return res.status(500).json(error);
     }
 }
+
+export const postSolicitudAliado = async(req,res)=>{
+    try{
+
+        const {id_cliente,denominacion,num_transferencia,num_cheque,num_tarjetaTDD,tdd_vencimiento,num_tarjetaTDC,
+            tdc_vencimiento,tipo_metodoP,rif, proyecto_id, mineral_id,
+            recurso_id, cargo_id, cantidad, obs
+        } = req.body;
+
+        const result = await pool.query(`select * from agregar_metodo ($1,$2,$3,$4,$5,$6,$7,$8,$9)
+            `,[id_cliente,denominacion,num_transferencia,num_cheque,num_tarjetaTDD,tdd_vencimiento,num_tarjetaTDC,
+                tdc_vencimiento,tipo_metodoP]);
+
+            const metodoId = result.rows[0].agregar_metodo;
+                console.log(metodoId);
+            await pool.query('CALL insertar_sol_aliado($1,$2,$3,$4,$5,$6,$7,$8)',
+                                                        [rif,proyecto_id,mineral_id,recurso_id,cargo_id,
+                                                        cantidad,obs,metodoId]);
+
+      return res.status(200).json({ message: 'Solicitud generada correctamente' });
+    }catch(error){
+        return res.status(500).json(error);
+    }
+}
+
+export const putSolicitudAliado = async (req,res)=>{
+    try{
+        
+    }catch(error){
+        return res.status(500).json(error);
+    }
+}
--- BackEnd/src/routes/solicitudes.routes.js
+++ BackEnd/src/routes/solicitudes.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { postSolicitudCliente } from '../controllers/solicitudes.controllers.js';
+import { postSolicitudCliente, postSolicitudAliado } from '../controllers/solicitudes.controllers.js';
 
 const router = Router();
 //Rutas
 
 router.post('/crear-solicitud-cliente', postSolicitudCliente);
+
+router.post('/crear-solicitud-aliado',postSolicitudAliado);
 export default router;
