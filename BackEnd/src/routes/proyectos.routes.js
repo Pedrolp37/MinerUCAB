5be77f6ff3e239d1c3aa33cfacd6bda87beac6a5
Pedrolp37 <56178798+Pedrolp37@users.noreply.github.com>
2024-07-07 19:47:37 -0400

    get proyecto eespecifico

--- BackEnd/src/routes/proyectos.routes.js
+++ BackEnd/src/routes/proyectos.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import {getProyecto} from '../controllers/proyectos.controllers.js';
 
 const router = Router();
 //Rutas
+router.get('/proyecto',getProyecto);
+
 export default router;
