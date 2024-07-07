51638a006956ed6e181053c81bf1459aa9d40baf
paoascanio <131598223+paoascanio@users.noreply.github.com>
2024-07-07 09:13:19 -0500

    FT-INSERT-SOLI_ALIADO-PA

---
+++ Scripts/FT-SCRIPTS-SOLI_ALIADO-PA.sql
@@ -0,0 +1,150 @@
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2023-06-01', 1250, 100, 'Pedido de minerales', 'J2310001238', 1, 1, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2023-07-15', 3500, 500.00, 'Solicitud de Pedido de minerales', 'J1590004598', 2, 12, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2023-08-20', 750.00, 75.00, 'Pedido del aliado', 'J1100001240', 3, 7, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2023-09-05', 2100.75, 150.25, 'Solicitud de materiales para nueva etapa', 'J1250001241', 4, null, 5, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2023-10-12', 1800.00, 120.00, 'Pedido para el proyecto', 'J1991001242', 5, 3, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2023-11-01', 4500.00, 300.00, 'Solicitud de varias cosas para proyecto', 'J0000001243', 6, 15, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2023-12-15', 1450.25, 85.50, 'Pedido de recursos', 'J0000005897', 7, 6, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-01-20', 3200.00, 400.00, 'Solicitud de recursos', 'J1000002992', 8, 11, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-02-28', 2750.00, 175.00, 'Pedido de insumos de minerales', 'J1560201244', 9, 4, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-03-10', 1900.75, 130.25, 'Pedido para el proyecto', 'J1690001215', 10, 8, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-04-05', 2400.00, 160.00, 'Pedido para el proyecto', 'J1200002874', 11, 2, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-05-15', 3800.00, 250.00, 'Solicitud de minerales', 'J1580001247', 12, 14, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-01', 1600.50, 95.00, 'Pedido para el proyecto', 'J6980005841', 13, 5, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-25', 2900.00, 350.00, 'Solicitud de minerales', 'J0000001259', 14, 10, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-25', 2900.00, 350.00, 'Solicitud de minerales', 'J0000001259', 14, 10, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 2100.75, 20, 'Solicitud de materiales para nueva etapa', 'J1250001241', 4, null, null, 22);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 5100.75, 150, 'Solicitud de materiales para nueva etapa', 'J1250001241', 6, null, null, 21);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 1100.75, 8, 'Solicitud de materiales para nueva etapa', 'J1250001241', 8, null, null, 20);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 2000, 200, 'Solicitud de materiales para nueva etapa', 'J1250001241', 10, null, null, 19);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-01', 1800, 180, 'Solicitud de materiales para nueva etapa', 'J1250001241', 1, null, null, 1);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-02', 1900, 190, 'Solicitud de materiales para nueva etapa', 'J0000001259', 10, null, null, 2);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-03', 2100, 210, 'Solicitud de materiales para nueva etapa', 'J0000001259', 32, null, null, 3);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-04', 1950, 195, 'Solicitud de materiales para nueva etapa', 'J1200002874', 20, null, null, 4);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 2100, 210, 'Solicitud de materiales para nueva etapa', 'J1590004598', 18, null, null, 5);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-06', 1850, 185, 'Solicitud de materiales para nueva etapa', 'J1000002992', 5, null, null, 6);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-07', 1950, 195, 'Solicitud de materiales para nueva etapa', 'J1585076312', 2, null, null, 7);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-08', 2000, 200, 'Solicitud de materiales para nueva etapa', 'J1680001281', 21, null, null, 8);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-09', 1900, 190, 'Solicitud de materiales para nueva etapa', 'J1690001244', 36, null, null, 9);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-10', 2050, 205, 'Solicitud de materiales para nueva etapa', 'J2992251275', 5, null, null, 10);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-11', 1800, 180, 'Solicitud de materiales para nueva etapa', 'J2500005841', 12, null, null, 19);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-12', 1950, 195, 'Solicitud de materiales para nueva etapa', 'J1350005890', 10, null, null, 18);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-13', 2100, 210, 'Solicitud de materiales para nueva etapa', 'J1000002992', 15, null, null, 16);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-14', 1900, 190, 'Solicitud de materiales para nueva etapa', 'J2130005841', 9, null, null, 15);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-15', 2000, 200, 'Solicitud de materiales para nueva etapa', 'J5890001288', 10, null, null, 14);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-03-10', 1900.75, 13, 'Pedido para el proyecto', 'J1690001215', 20, 28, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-04-05', 2400.00, 20, 'Pedido para el proyecto', 'J1200002874', 12, 22, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-05-15', 3800.00, 250, 'Solicitud de minerales', 'J1580001247', 5, 14, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-01', 1600.50, 95, 'Pedido para el proyecto', 'J6980005841', 2, 15, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-25', 2900.00, 350, 'Solicitud de minerales', 'J0000001259', 4, 20, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-25', 2900.00, 380, 'Solicitud de minerales', 'J0000001259', 8, 20, null, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 2100.75, 20, 'Solicitud de materiales para nueva etapa', 'J1250001241', 4, null, null, 22);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 5100.75, 150, 'Solicitud de materiales para nueva etapa', 'J1250001241', 6, null, null, 21);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 1100.75, 8, 'Solicitud de materiales para nueva etapa', 'J1250001241', 8, null, null, 20);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 2000, 200, 'Solicitud de materiales para nueva etapa', 'J1250001241', 10, null, null, 19);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-01', 1800, 180, 'Solicitud de materiales para nueva etapa', 'J1250001241', 1, null, null, 1);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-02', 1900, 190, 'Solicitud de materiales para nueva etapa', 'J0000001259', 10, null, null, 2);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-03', 2100, 210, 'Solicitud de materiales para nueva etapa', 'J0000001259', 32, null, null, 3);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-04', 1950, 195, 'Solicitud de materiales para nueva etapa', 'J1200002874', 20, null, 1, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-05', 2100, 210, 'Solicitud de materiales para nueva etapa', 'J1590004598', 18, null, 2, null);
+
+INSERT INTO SOLICITUD_ALIADO (factura_ali_fecha, factura_ali_total, factura_ali_cantidad, factura_ali_observacion, factura_fk_ali_RIF, factura_fk_pro_id, factura_ali_min_id, factura_ali_tire_id, factura_ali_carg_id)
+VALUES ('2024-06-06', 1850, 185, 'Solicitud de materiales para nueva etapa', 'J1000002992', 5, null, 3, null);
+
