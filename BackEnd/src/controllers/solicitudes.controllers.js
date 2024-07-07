6a5e813b431bc6eb1ab7be92c204a8fcc1019e27
Pedrolp37 <56178798+Pedrolp37@users.noreply.github.com>
2024-07-07 01:53:41 -0400

    sol cliente

--- BackEnd/src/controllers/solicitudes.controllers.js
+++ BackEnd/src/controllers/solicitudes.controllers.js
@@ -4,18 +4,20 @@ import {pool} from '../databases/BD_Connection.js';
 export const postSolicitudCliente = async(req,res) =>{
     try{   
         const {id_cliente,denominacion,num_transferencia,num_cheque,num_tarjetaTDD,tdd_vencimiento,num_tarjetaTDC,
-            tdc_vencimiento,tipo_metodoP} = req.body;
+            tdc_vencimiento,tipo_metodoP,
+            mineral_id, cantidad,obs} = req.body;
         const result = await pool.query(`select * from agregar_metodo ($1,$2,$3,$4,$5,$6,$7,$8,$9)
                                         `,[id_cliente,denominacion,num_transferencia,num_cheque,num_tarjetaTDD,tdd_vencimiento,num_tarjetaTDC,
                                             tdc_vencimiento,tipo_metodoP]);
-          //  await pool.query('')
-        console.log(result);
+          
+       
           const metodoId = result.rows[0].agregar_metodo;
+            
+               await pool.query('CALL insertar_sol_cliente($1::VARCHAR,$2::INT,$3::INT,$4::VARCHAR,$5::INT)',[id_cliente,mineral_id,cantidad,obs,
+                                                                                metodoId]); 
 
-        // Usa el ID como necesites en tu lógica de front-end
-        // ...
+        return res.status(200).json({ message: 'Solicitud generada correctamente' });
 
-        return res.status(200).json({ metodoId });
     }catch(error){
         return res.status(500).json(error);
     }
