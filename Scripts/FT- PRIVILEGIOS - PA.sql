488c4009ce6555100ca4cb0bc0ebc9f9cde5625c
paoascanio <131598223+paoascanio@users.noreply.github.com>
2024-07-07 20:06:25 -0500

    FT- PRIVILEGIOS- PA

---
+++ Scripts/FT- PRIVILEGIOS - PA.sql
@@ -0,0 +1,28 @@
+GRANT SELECT, INSERT, UPDATE ON actividad, actividad_cargo, actividad_ej, actividad_estatus, actividad_recurso, 
+aliado_Comercial, cargo, cargo_ej, cargo_empleado, cliente, concesion, recurso, recurso_ej, recurso_estatus,
+recurso_material, proyecto, pro_estatus, mineral, mineral_pozo, yacimiento, min_yacimiento, horario, horario_empleado, 
+empleado TO paola_developer;
+
+GRANT SELECT, INSERT, UPDATE ON actividad, actividad_cargo, actividad_ej, actividad_estatus, actividad_recurso, 
+aliado_Comercial, cargo, cargo_ej, cargo_empleado, cliente, concesion, recurso, recurso_ej, recurso_estatus,
+recurso_material, proyecto, pro_estatus, mineral, mineral_pozo, yacimiento, min_yacimiento ,
+costo_actividad, costo_humano, inventario, metodo_pago, horario, horario_empleado, empleado,
+solicitud_aliado, solicitud_cliente TO pedro_developer;
+
+GRANT SELECT, INSERT, UPDATE ON actividad, actividad_cargo, actividad_ej, actividad_estatus, actividad_recurso, 
+aliado_Comercial, cargo, cargo_ej, cargo_empleado, cliente, concesion, recurso, recurso_ej, recurso_estatus,
+recurso_material, proyecto, pro_estatus, mineral, mineral_pozo, yacimiento, min_yacimiento ,
+costo_actividad, costo_humano, inventario, metodo_pago, horario, horario_empleado, empleado,
+solicitud_aliado, solicitud_cliente, tarjeta_credito, tarjeta_debito, cheque, transferencia,
+metodo_pago, efectivo TO arturo_developer;
+
+GRANT SELECT ON  actividad, actividad_cargo, actividad_ej, actividad_estatus, actividad_recurso, cargo, cargo_ej, cargo_empleado, 
+cliente, concesion, recurso, recurso_ej, recurso_estatus,
+recurso_material, proyecto, pro_estatus, mineral, mineral_pozo, yacimiento, min_yacimiento,
+costo_actividad, costo_humano, inventario, horario, horario_empleado, empleado TO coordinador;
+
+GRANT SELECT, INSERT, UPDATE ON actividad, actividad_cargo, actividad_ej, actividad_estatus, actividad_recurso, 
+aliado_Comercial, cargo, cargo_ej, cargo_empleado, cliente, concesion, recurso, recurso_ej, recurso_estatus,
+recurso_material, proyecto, pro_estatus, mineral, mineral_pozo, yacimiento, min_yacimiento ,
+costo_actividad, costo_humano, inventario, metodo_pago, horario, horario_empleado, empleado,
+solicitud_aliado, solicitud_cliente TO jefe;
